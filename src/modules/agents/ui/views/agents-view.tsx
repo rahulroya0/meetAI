d51525ad85b5fd64809148cf3accff234485fd6f
RahulRoy <royrahula0@gmail.com>
2026-01-28 21:33:59 +0530

    12: agents data table

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -6,14 +6,26 @@ import { ResponsiveDialog } from "@/components/responsive-dialog";
 import { Button } from "@/components/ui/button";
 import { useTRPC } from "@/trpc/client"
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { DataTable } from "../components/data-table";
+import { columns, Payment } from "../components/columns";
+import { useTransition } from "react";
+import { EmptyState } from "@/components/empty-state";
 
 export const AgentsView = () => {
-  const trpc = useTRPC();
-  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
+
+  const trpc=useTRPC();
+  const {data}= useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
   return (
-    <div>
-      {JSON.stringify(data, null, 2)}
+    <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
+      <DataTable data={data} columns={columns}/>
+      {data.length===0 && (
+        <EmptyState 
+        title="Create your first agent"
+        description="Create an agent to join your meetings. Each agent will follow your instructions and can
+        interact with participants during the call."
+        />
+      )}
     </div>
   )
 }
